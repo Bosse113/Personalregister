2e78283af42bcff30a19551675af0e94e8cab872
Bosse113 <49038474+Bosse113@users.noreply.github.com>
2026-05-20 16:10:32 +0200

    Lade till metodernas innehåll och ToString override

--- Personalregister/Program.cs
+++ Personalregister/Program.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Win32;
 using System.Collections.Generic;
+
 // Uppgift 1: Klasserna bör vara personal . Detta räcker då det bara är namn och lön
 // Uppgift 2: Namn och lön 
+
 namespace Personalregister
 {
     public class Personal
@@ -15,20 +17,27 @@ namespace Personalregister
             Namn = namn;
             Lon = lon;
         }
-
-        
-
+        // Lösningen nedan hittade jag efter googling, Jag förstår inte varför det blir konstigt resultat utan den??
+        // --- Personal lista ---
+        // Personalregister.Personal
+        public override string ToString()
+        {
+            return $"Namn: {Namn}, Lön: {Lon}";
+        }
 
     }
     internal class Program
     {
-        
+        // Skapa Lista med personalen för att enkelt kunna använda add m.m.
+        // Måste ligga utanför Main , antagligen pga att den ska vara tillgänglig 
+        static List<Personal> register = new List<Personal>();
+
 
         static void Main(string[] args)
         {
             bool menyRun = true;
 
-            while (menyRun==true)
+            while (menyRun == true)
             {
                 // MENY
                 Console.WriteLine("--- Personalregister ---");
@@ -60,13 +69,34 @@ namespace Personalregister
             }
             static void LaggTillPerson()
             {
-               
-                
+                Console.Write("Ange namn: ");
+                string namn = Console.ReadLine();
+
+                Console.Write("Ange lön: ");
+                int lon = int.Parse(Console.ReadLine()); //Gör om inmatningens string till Int.
+
+                register.Add(new Personal(namn, lon));
+
+                Console.WriteLine("Person tillagd.");
+
+
             }
 
             static void VisaPersoner()
             {
-               
+                Console.WriteLine("\n--- Personal lista ---");
+
+                if (register.Count == 0) //Kolla om registret är tomt
+                {
+                    Console.WriteLine("Inga personer registrerade.");
+                    return;
+                }
+                //Lista alla poster
+                foreach (Personal person in register)
+                {
+                    Console.WriteLine(person.ToString()); //Skriv ut med den "nya" ToString
+
+                }
             }
         }
     }
